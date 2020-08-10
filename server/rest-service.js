9e151dcf624303cc90612c795cc9b6897a966fc1
Shankhajit Tat <shankhajit.tat@gmail.com>
2020-08-10 17:07:06 +0100

    Update rest-service.js

--- server/rest-service.js
+++ server/rest-service.js
@@ -1,5 +1,5 @@
 function createContactRest(req,res,next){
-  console.log('Request Body'+req.body);
+  console.log('Request Body'+JSON.stringify(req.body));
   res.status(201).end();
 }
 exports.createContactRest = createContactRest;
